87867ffe82eeca662d9c7da4af539dd898e579bc
mayur70 <mayurpatil70@gmail.com>
2025-03-23 22:54:45 +0530

    vsync + version log

--- main.c
+++ main.c
@@ -21,6 +21,9 @@ int main() {
 	if(!glfwInit())
 		die("failed to init glfw");
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	window = glfwCreateWindow(800, 600, "edit-gl", NULL, NULL);
 	if(window == NULL)
 		die("glfwCreateWindow");
@@ -28,6 +31,12 @@ int main() {
 	if(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0)
 		die("gladLoadGLLoader");
 
+	printf("openGL initialized.\n\tversion: %s\n\trenderer: %s\n\tvendor: %s\n",
+			glGetString(GL_VERSION),
+			glGetString(GL_RENDERER),
+			glGetString(GL_VENDOR));
+
+	glfwSwapInterval(1);
 	glfwSetKeyCallback(window, on_key_event);
 	while(!glfwWindowShouldClose(window)) {
 
