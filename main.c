58b8bbf7fde827376a1e37f6273a22770514bd85
mayur70 <mayurpatil70@gmail.com>
2025-03-24 21:56:31 +0530

    gl triangle

--- main.c
+++ main.c
@@ -5,7 +5,24 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+const char* vertex_src = "#version 330 core\n"
+	"layout (location= 0) in vec3 apos;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(apos, 1.0);\n"
+	"}\0";
+	
+const char* fragment_src = "#version 330 core\n"
+	"out vec4 frag_color;\n"
+	"void main()\n"
+	"{\n"
+	"	frag_color = vec4(1.0, 0.5, 0.2, 1.0);\n"
+	"}\0";
+
 GLFWwindow* window = NULL;
+unsigned int vbo = 0;
+unsigned int vao = 0;
+unsigned int program = 0;
 
 void die(const char* msg) {
 	perror(msg);
@@ -38,10 +55,61 @@ int main() {
 
 	glfwSwapInterval(1);
 	glfwSetKeyCallback(window, on_key_event);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	float vertices[] = {
+		-0.5, -0.5, 0.0,
+		0.5, -0.5, 0.0,
+		0.0, 0.5, 0.0
+	};
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_src, NULL);
+	glCompileShader(vs);
+	int suc;
+	char infolog[512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &suc);
+	if(suc == 0){
+		glGetShaderInfoLog(vs, 512, NULL, infolog);
+		die(infolog);
+	}
+	unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_src, NULL);
+	glCompileShader(fs);
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &suc);
+	if(suc == 0) {
+		glGetShaderInfoLog(fs, 512, NULL, infolog);
+		die(infolog);
+	}
+	program = glCreateProgram();
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &suc);
+	if(suc == 0) {
+		glGetProgramInfoLog(program, 512, NULL, infolog);
+		die(infolog);
+	}
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	while(!glfwWindowShouldClose(window)) {
 
 		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(program);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
